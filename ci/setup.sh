b440e454e70089a57def73ed6f42c90786bc0c44
H-Richard <r.hong@pm.me>
2021-03-20 17:59:13 -0400

    chore: remove notebooks, enforce light mode (for now)

--- ci/setup.sh
+++ ci/setup.sh
@@ -10,4 +10,10 @@ echo "\
 headless = true\n\
 enableCORS = false\n\
 port = $PORT\n\
+[theme]\n\
+primaryColor=\"#F63366\"\n\
+backgroundColor=\"#FFFFFF\"\n\
+secondaryBackgroundColor=\"#F0F2F6\"\n\
+textColor=\"#262730\"\n\
+font=\"sans serif\"\n\
 " > ~/.streamlit/config.toml
